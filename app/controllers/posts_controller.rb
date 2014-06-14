b2561cdd5aa2440bdb056f06b3a0a77a115a1a02
Travis Riggs <travis.a.riggs@gmail.com>
2014-06-13 20:47:14 -0400

    * Modified PostController to setup instance variables for posts * Modified posts index and show views to use model data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
-  
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comments = Comment.all
   end
 
   def create
