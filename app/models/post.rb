9992e5c820a9dede56e805f33a8a9fbe7d0de2da
Travis Riggs <travis.a.riggs@gmail.com>
2014-06-16 17:00:09 -0400

    * Changed reference in the model from 'user' to 'creator' for posts. Had to alter the view that references the author of the post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   #  conventions to relate things together
   #belongs_to :user
   # Could use the more explicit form for the relationship
-  belongs_to :user, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
