f698652c1e6bf9357109455687e94504c4204c70
Travis Riggs <travis.a.riggs@gmail.com>
2014-06-27 16:25:46 -0400

    * Added sub-resources for comments on posts in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, except: :destroy
+  resources :posts, except: :destroy do
+    resources :comments, only: [:new, :create, :edit, :update]
+  end
 
 end
