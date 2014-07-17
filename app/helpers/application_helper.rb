8f51ef8807904d495eab82dbb43677cec0c07695
Travis Riggs <travis.a.riggs@gmail.com>
2014-07-17 13:54:43 -0400

    * Created helper method to ensure that 'http://' is included before URL's if it's not already there * Created helper method to format timestamps in a more human readable form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def fix_url(url)
+    if url.starts_with?('http://')
+      url
+    else
+      'http://' + url
+    end
+  end
+
+  def format_time(timestamp)
+    # Example Format:  Tuesday, January 10, 2014 at 8:42 pm
+    format_str = '%A, %B %-d, %Y at %-l:%M %P'
+    timestamp.strftime(format_str)
+  end
+
 end
