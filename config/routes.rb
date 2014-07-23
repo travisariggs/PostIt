74200cde0480a1dc1b6379944b599bbbf6771c86
Travis Riggs <travis.a.riggs@gmail.com>
2014-07-23 17:07:18 -0400

    * Added custom routes to login and register users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
   resources :posts, except: :destroy do
     resources :comments, only: [:create]
   end
