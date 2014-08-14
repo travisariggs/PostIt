34f65ef16f344a958e31b7982cf5ce36d2cef70d
Travis Riggs <travis.a.riggs@gmail.com>
2014-08-14 13:07:49 -0400

    * Added validations to new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :comments
 
   has_secure_password validations: false
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, on: :create, presence: true, length: {minimum: 4}
+
 end
