4b3ef90875b45400334698f08cf13c3267993f50
Travis Riggs <travis.a.riggs@gmail.com>
2014-08-19 14:06:22 -0400

    * Added user login authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      flash[:notice] = "Welcome back #{user.username}!"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = 'There was something wrong with your user name or password'
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
