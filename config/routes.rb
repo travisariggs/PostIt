7921c1ba24255cf850a0d66fe0c045a48c30d417
Travis Riggs <travis.a.riggs@gmail.com>
2014-08-19 14:07:28 -0400

    * No real changes. Just whitespace.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ PostitTemplate::Application.routes.draw do
   get '/register', to: 'users#new', as: 'register'
   post '/register', to: 'users#create'
 
-
   resources :posts, except: :destroy do
     resources :comments, only: [:create]
   end
