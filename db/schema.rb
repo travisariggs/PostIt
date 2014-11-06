6b65ac66ea7a44aadffe81ba28a0fae5cb50bb24
Travis Riggs <travis.a.riggs@gmail.com>
2014-11-05 21:25:23 -0500

    * Added a string to hold user's timezone to th users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723202856) do
+ActiveRecord::Schema.define(version: 20141106022006) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140723202856) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "timezone_string"
   end
 
 end
