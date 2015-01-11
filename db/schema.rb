bcf80a7373e5658587207ae13f90c6a46944a31a
Travis Riggs <travis.a.riggs@gmail.com>
2015-01-11 15:37:36 -0500

    * Added column to Posts table for slugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111195019) do
+ActiveRecord::Schema.define(version: 20150111202002) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141111195019) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "slug"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
