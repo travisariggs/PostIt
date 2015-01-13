645983300912f5b9cc927fc34ec0192b8335ad3f
Travis Riggs <travis.a.riggs@gmail.com>
2015-01-13 16:48:21 -0500

    * Created migration to add slugs to categories and users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111202002) do
+ActiveRecord::Schema.define(version: 20150113214603) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "comments", force: true do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150111202002) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "timezone_string"
+    t.string   "slug"
   end
 
   create_table "votes", force: true do |t|
