f3790bc1b7c12a1fdaed495cb3bae635765323ad
Travis Riggs <travis.a.riggs@gmail.com>
2015-01-21 13:03:42 -0500

    * Created helper methods to check if user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :admin?
 
   def current_user
     if session[:user_id]
@@ -17,9 +17,24 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def admin?
+    if logged_in?
+      current_user.role == 'admin'
+    else
+      false
+    end
+  end
+
   def require_user
     if not logged_in?
       flash[:error] = 'You have to be logged in to do that!'
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if not admin?
+      flash[:error] = 'Only the boss can do that...'
       redirect_to root_path
     end
   end
