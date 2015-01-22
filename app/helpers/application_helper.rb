83917386642b8423431207eddcb26170b6db9476
Travis Riggs <travis.a.riggs@gmail.com>
2015-01-22 15:35:16 -0500

    * Modified the format_string helper to allow an optional arg to set the timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,10 @@ module ApplicationHelper
     end
   end
 
-  def format_time(timestamp)
+  def format_time(timestamp, timezone=Time.zone.name)
     # Example Format:  Tuesday, January 10, 2014 at 8:42 pm
     format_str = '%A, %B %-d, %Y at %-l:%M %P'
-    timestamp.strftime(format_str)
+    timestamp.in_time_zone(timezone).strftime(format_str)
   end
 
 end
